316181852c6fda1265ce2ea0c7bfe14415288293
Jim Veneskey <github@strifecrafter.com>
2015-05-17 09:07:45 -0400

    Created outlets and actions for all of the buttons and labels

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlets (labels) from the UI
+    @IBOutlet weak var moneySupplyCount: UILabel!
+    @IBOutlet weak var lemonSupplyCount: UILabel!
+    @IBOutlet weak var iceCubeSupplyCount: UILabel!
+    @IBOutlet weak var lemonPurchaseCount: UILabel!
+    @IBOutlet weak var iceCubePurchaseCount: UILabel!
+    @IBOutlet weak var lemonMixCount: UILabel!
+    @IBOutlet weak var iceCubeMixCount: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // IBActions (Buttons) from the UI
+    
+    @IBAction func purchaseLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func purchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unpurchaseLemonButtonPressed(sender: UIButton) {
+    }
 
+    @IBAction func unpurchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixLemonButtonPressed(sender: UIButton) {
+    }
+    @IBAction func mixIceCubeButtonPressed(sender: UIButton) {
+    }
+   
+    @IBAction func unmixLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unMixIceCubeButtonPressed(sender: UIButton) {
+    }
+
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
 }
 
