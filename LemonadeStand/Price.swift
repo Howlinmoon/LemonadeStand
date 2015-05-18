074caa1f974e5bca9d04bb46b60515a089a2bf83
Jim Veneskey <github@strifecrafter.com>
2015-05-18 12:12:28 -0400

    Added another structure to handle our pricing model

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,15 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by jim Veneskey on 5/18/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    // our fixed prices
+    let lemon = 2
+    let iceCube = 1
+}
